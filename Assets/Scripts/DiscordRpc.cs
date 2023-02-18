7e85466985682f9cddfe5ec61d6d47b1e637211c
wararyo <wararyo@gmail.com>
2023-02-19 03:15:01 +0900

    DiscordRPCを仮実装

---
+++ Assets/Scripts/DiscordRpc.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using Cysharp.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class DiscordRpc : MonoBehaviour
+{
+
+    public string clientId;
+    public string clientSecret;
+    public string[] scopes;
+
+    IpcClient client;
+    public bool isConnected { get; private set; } = false;
+
+    async void Start()
+    {
+        //var cancellationToken = this.GetCancellationTokenOnDestroy();
+        client = new IpcClient();
+
+        await client.Connect();
+        isConnected = true;
+        Debug.Log(await Handshake());
+        var authorizeData = await Authorize();
+        string code = authorizeData.data["data"]?["code"]?.Value<string>();
+        if (code == null) throw new Exception("Failed to get code.");
+        Debug.Log(code);
+        string accessToken = await FetchAccessToken(code);
+        Debug.Log(await Authenticate(accessToken));
+        bool? muted = await GetMute();
+        await SetMute((!muted) ?? true);
+        Debug.Log(await Subscribe("VOICE_SETTINGS_UPDATE"));
+        Debug.Log(await client.Receive());
+    }
+
+    void OnDestroy()
+    {
+        if (client != null)
+            client.Dispose();
+    }
+
+    async UniTask<IpcPacket> SendForRequest(IpcPacket packet)
+    {
+        await client.Send(packet);
+        return await client.Receive();
+    }
+
+    public async UniTask<IpcPacket> Handshake()
+    {
+        JObject handshake = new JObject
+        {
+            ["v"] = 1,
+            ["client_id"] = clientId
+        };
+        IpcPacket packet = new IpcPacket(IpcPacket.Opcodes.HANDSHAKE, handshake);
+        return await SendForRequest(packet);
+    }
+
+    public async UniTask<IpcPacket> Authorize()
+    {
+        JObject content = new JObject
+        {
+            ["cmd"] = "AUTHORIZE",
+            ["args"] = new JObject
+            {
+                ["scopes"] = new JArray(scopes),
+                ["client_id"] = clientId
+            },
+            ["nonce"] = Nonce()
+        };
+        return await SendForRequest(new IpcPacket(IpcPacket.Opcodes.FRAME, content));
+    }
+
+    public async UniTask<string> FetchAccessToken(string code)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("client_id", clientId);
+        form.AddField("client_secret", clientSecret);
+        form.AddField("code", code);
+        form.AddField("grant_type", "authorization_code");
+        form.AddField("redirect_url", "http://localhost");
+        var request = UnityWebRequest.Post("https://discord.com/api/oauth2/token", form);
+        await request.SendWebRequest();
+        JObject json = JObject.Parse(request.downloadHandler.text);
+        if (json["access_token"] == null) throw new Exception();
+        return json["access_token"].Value<string>();
+    }
+
+    public async UniTask<IpcPacket> Authenticate(string accessToken)
+    {
+        JObject content = new JObject
+        {
+            ["cmd"] = "AUTHENTICATE",
+            ["args"] = new JObject
+            {
+                ["access_token"] = accessToken
+            },
+            ["nonce"] = Nonce()
+        };
+        return await SendForRequest(new IpcPacket(IpcPacket.Opcodes.FRAME, content));
+    }
+
+    public async UniTask<bool?> GetMute()
+    {
+        JObject content = new JObject
+        {
+            ["cmd"] = "GET_VOICE_SETTINGS",
+            ["args"] = new JObject { },
+            ["nonce"] = Nonce()
+        };
+        var setting = await SendForRequest(new IpcPacket(IpcPacket.Opcodes.FRAME, content));
+        return setting.data["data"]?["mute"]?.Value<bool>();
+    }
+
+    public async UniTask<IpcPacket> SetMute(bool mute)
+    {
+        JObject content = new JObject
+        {
+            ["cmd"] = "SET_VOICE_SETTINGS",
+            ["args"] = new JObject
+            {
+                ["mute"] = mute
+            },
+            ["nonce"] = Nonce()
+        };
+        return await SendForRequest(new IpcPacket(IpcPacket.Opcodes.FRAME, content));
+    }
+
+    public async UniTask<IpcPacket> Subscribe(string evt)
+    {
+        JObject content = new JObject
+        {
+            ["cmd"] = "SUBSCRIBE",
+            ["args"] = new JObject { },
+            ["evt"] = evt,
+            ["nonce"] = Nonce()
+        };
+        return await SendForRequest(new IpcPacket(IpcPacket.Opcodes.FRAME, content));
+    }
+
+    public async UniTask<IpcPacket> Close()
+    {
+        Debug.Log(new IpcPacket(IpcPacket.Opcodes.CLOSE, new JObject()));
+        return await SendForRequest(new IpcPacket(IpcPacket.Opcodes.CLOSE, new JObject()));
+    }
+
+    string Nonce()
+    {
+        return Guid.NewGuid().ToString();
+    }
+}
---
+++ Assets/Scripts/IPC.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Text;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class IpcClient
+{
+    private NamedPipeClientStream stream;
+
+    string GetPipeName(int id)
+    {
+        return $"discord-ipc-{id}";
+    }
+
+    public async UniTask Connect()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            try
+            {
+                stream = new NamedPipeClientStream(".", GetPipeName(i));
+                await UniTask.RunOnThreadPool(() => stream.Connect());
+                Debug.Log($"IPC Connected: " + GetPipeName(i));
+                break;
+            }
+            catch (Exception e)
+            {
+                if (i == 9)
+                {
+                    Debug.LogError("Failed to connect IPC: " + e.Message);
+                    return;
+                }
+                continue;
+            }
+        }
+    }
+
+    public async UniTask Send(IpcPacket packet)
+    {
+        if (stream == null)
+            throw new InvalidOperationException();
+        var bytes = packet.ToBytes();
+        await UniTask.RunOnThreadPool(() =>
+        {
+            stream.Write(bytes, 0, bytes.Length);
+        });
+    }
+
+    public async UniTask<IpcPacket> Receive()
+    {
+        if (stream == null)
+            throw new InvalidOperationException();
+        var buffer = new byte[4096];
+        var len = await stream.ReadAsync(buffer, 0, buffer.Length);
+        return new IpcPacket(buffer, len);
+    }
+
+    public void Dispose()
+    {
+        if (stream != null) stream.Dispose();
+    }
+}
+
+public class IpcPacket
+{
+    public enum Opcodes
+    {
+        HANDSHAKE = 0x0000,
+        FRAME = 0x0001,
+        CLOSE = 0x0002,
+        PING = 0x0003,
+        PONG = 0x0004
+    }
+
+    public IpcPacket(Opcodes opcode, JObject data)
+    {
+        this.opcode = opcode;
+        this.data = data;
+    }
+    public IpcPacket(byte[] bytes, int size)
+    {
+        opcode = (Opcodes)BitConverter.ToUInt32(bytes, 0);
+        uint length = BitConverter.ToUInt32(bytes, sizeof(uint));
+        if (sizeof(uint) + sizeof(uint) + length > size) throw new Exception();
+        string jsonString = Encoding.UTF8.GetString(bytes, sizeof(uint) + sizeof(uint), (int)length);
+        data = JObject.Parse(jsonString);
+    }
+    public byte[] ToBytes()
+    {
+        byte[] json = Encoding.UTF8.GetBytes(data.ToString(Formatting.None));
+        byte[] op = BitConverter.GetBytes((uint)opcode);
+        byte[] len = BitConverter.GetBytes(json.Length);
+
+        byte[] buff = new byte[op.Length + len.Length + json.Length];
+        op.CopyTo(buff, 0);
+        len.CopyTo(buff, op.Length);
+        json.CopyTo(buff, op.Length + len.Length);
+        return buff;
+    }
+    public override string ToString()
+    {
+        return $"[Opcode: {opcode.ToString()}, Data: {data.ToString()}]";
+    }
+
+    public Opcodes opcode;
+    public JObject data;
+}
